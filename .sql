ce7212485646de8bc5c6fbee4fca499183c872aa
alokkumar92 <132120340+alokkumar92@users.noreply.github.com>
2024-10-04 15:30:45 +0530

    Add files via upload

---
+++ .sql
@@ -0,0 +1,39 @@
+--------------------------------------------------------
+--  File created - Friday-October-04-2024   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table PRODUCT
+--------------------------------------------------------
+
+  CREATE TABLE "HR"."PRODUCT" 
+   (	"P_ID" NUMBER(*,0), 
+	"P_NAME" VARCHAR2(50 BYTE)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+REM INSERTING into HR.PRODUCT
+SET DEFINE OFF;
+Insert into HR.PRODUCT (P_ID,P_NAME) values (1022,'iphone');
+Insert into HR.PRODUCT (P_ID,P_NAME) values (1023,'sam');
+Insert into HR.PRODUCT (P_ID,P_NAME) values (1024,'vivo');
+Insert into HR.PRODUCT (P_ID,P_NAME) values (1025,'realme');
+--------------------------------------------------------
+--  DDL for Index SYS_C007733
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "HR"."SYS_C007733" ON "HR"."PRODUCT" ("P_ID") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
+--------------------------------------------------------
+--  Constraints for Table PRODUCT
+--------------------------------------------------------
+
+  ALTER TABLE "HR"."PRODUCT" ADD PRIMARY KEY ("P_ID")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS"  ENABLE;
